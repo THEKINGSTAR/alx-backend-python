6c84ddb7c6f9d8d3c56524ec5e72a1681df1e5c9
THEKINGSTAR <thekingstar5002@gmail.com>
2024-03-15 04:07:31 +0200

    update the repo

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+0. The basics of async
+"""
+
+
+import asynico
+
+
+async def wait_random(max_delay=10):
+    """
+    asynchronous coroutine that takes in an integer argument
+    """
+    random(0, max_delay)
+
+if __name__ == "__main__":
+    import time
+    s = time.pref_counter()
+    asyncio.run()
