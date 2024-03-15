dd79d44cc354b2ed4c843965058c36cac75c578a
THEKINGSTAR <thekingstar5002@gmail.com>
2024-03-15 04:06:06 +0200

    4. Define variables: fix documentation

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -16,14 +16,14 @@ i_understand_annotations, a boolean with a value of True
 school, a string with a value of “Holberton”
 """
 
-a: int = 1
 # a, an integer with a value of 1
+a: int = 1
 
-pi:  float = 3.14
 # pi, a float with a value of 3.14
+pi:  float = 3.14
 
-i_understand_annotations: bool = True
 # i_understand_annotations, a boolean with a value of True
+i_understand_annotations: bool = True
 
-school: str = "Holberton"
 # school, a string with a value of “Holberton”
+school: str = "Holberton"
