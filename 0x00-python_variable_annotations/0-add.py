04475ecc348e2e90703ad664901ae40d11601ab5
THEKINGSTAR <thekingstar5002@gmail.com>
2024-03-15 02:16:09 +0200

    CODE DUCOMENTATION

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+
 def add(a: float, b: float) -> float:
+    """
+    takes a float a and a float b
+    as arguments and returns their sum as a float.
+    """
     sum = a + b
     return sum
