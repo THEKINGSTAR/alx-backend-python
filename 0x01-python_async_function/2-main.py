a652275a5e05acf073e7b0110c5c15a71662f2c8
THEKINGSTAR <thekingstar5002@gmail.com>
2024-03-22 15:57:42 +0200

    2. Measure the runtime

---
+++ 0x01-python_async_function/2-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+measure_time = __import__('2-measure_runtime').measure_time
+
+n = 5
+max_delay = 9
+
+print(measure_time(n, max_delay))
+"""
+bob@dylan:~$ ./2-main.py
+1.759705400466919
+"""
