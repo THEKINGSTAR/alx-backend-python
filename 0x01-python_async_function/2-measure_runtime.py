5c265131bb694012b619a4b2e28434b79ff76726
THEKINGSTAR <thekingstar5002@gmail.com>
2024-03-22 16:03:41 +0200

    using asyncio.rum to run the ASYNC FUNCTION

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -26,6 +26,7 @@ def measure_time(n: int, max_delay: int) -> float:
     Your function should return a float.
     """
     start_time = time.time()
-    wait_n(n, max_delay)
-    total_time = time.time() - start_time
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
     return total_time / n
