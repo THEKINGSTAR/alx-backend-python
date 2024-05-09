64946f5ff4033b9bff84fdb3df25bb92fa9394f5
THEKINGSTAR <thekingstar5002@gmail.com>
2024-05-09 07:11:27 +0300

    UPDATE README.md FILE

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,27 +5,26 @@ MODULE TO TEST!
 
 from unittest.mock import MagicMock
 # from nose.tools import assert_equal
-# from parameterized import parameterized, parameterized_class
+from parameterized import parameterized
 import unittest
+from utils import access_nested_map
 import math
 
-
-
-@parameterized.expand([
-    {"a": 1}, "expected":"a")
-    {"a": {"b": 2}},  "expected":"a",
-    {"a": {"b": 2}},  "expected":"a", "b"
-])
 
 class TestAccessNestedMap(unittest.TestCase) :
     """
     class that inherits from unittest.TestCase
     """
-    @parameterized.expand
-    def test_access_nested_map(self):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expctd_rslts):
         """
         method to test that the method
         returns what it is supposed to.
         """
+        self.assertEqual(access_nested_map(nested_map, path), expctd_rslts)
         return 
 
