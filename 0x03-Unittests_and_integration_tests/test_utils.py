ffb7f1e6ada11ccbf42fa7c945e2299edea5bc31
THEKINGSTAR <thekingstar5002@gmail.com>
2024-05-18 01:31:49 +0300

    correct the function logic

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -52,7 +52,7 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-    def _test_get_json(self, test_url: str, test_payload: dict) -> None:
+    def test_get_json(self, test_url: str, test_payload: dict) -> None:
         """
         method to test that utils.get_json returns the expected result.
         Use unittest.mock.patch to patch requests.get.
