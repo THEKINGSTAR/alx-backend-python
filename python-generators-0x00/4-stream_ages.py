d0d9ba47661ad65cf3a2ac753f841dbf8fb3e0b2
THEKINGSTAR <thekingstar5002@gmail.com>
2025-07-12 00:47:41 +0300

    4. Memory-Efficient Aggregation with Generators

--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -3,7 +3,62 @@
 Implements lazy pagination from the user_data table using a generator.
 """
 
-data_base = __import__('seed').connect_to_prodev
+
+import os
+from dotenv import load_dotenv
+import mysql.connector
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the variables
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+
+def connect_db(include_database=True):
+    """
+    Connects to the MySQL server (optionally includes the database).
+    """
+    try:
+        config = {
+            "host": DB_HOST,
+            "user": DB_USER,
+            "password": DB_PASSWORD,
+        }
+        if include_database:
+            config["database"] = DB_NAME
+
+        connection = mysql.connector.connect(**config)
+
+        if connection.is_connected():
+            msg = (
+                f"Connected to MySQL database {DB_NAME}"
+                if include_database else
+                "Connected to MySQL server"
+            )
+            # print(msg)
+            return connection
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    return None
+
+
+def connect_to_prodev():
+    """
+    connects the the ALX_prodev database in MYSQL
+    """
+    temp_conn = connect_db(include_database=False)
+    if temp_conn:
+        # create_database(temp_conn)
+        temp_conn.close()
+        return connect_db(include_database=True)
+    else:
+        print("Failed to connect to MySQL server.")
+        return None
 
 
 def stream_user_ages():
@@ -16,7 +71,7 @@ def stream_user_ages():
     You must use no more than two loops in your script
     You are not allowed to use the SQL AVERAGE
     """
-    connection = data_base()
+    connection = connect_to_prodev()
     if not connection:
         return
 
