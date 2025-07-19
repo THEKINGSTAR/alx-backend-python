77fec4f9190dde9c6092ae43e6390d9451488d98
THEKINGSTAR <thekingstar5002@gmail.com>
2025-07-19 20:41:08 +0300

    1. Parameterize a unit test|

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -28,6 +28,23 @@ class TestAccessNestedMap(unittest.TestCase):
         returns what it is supposed to.
         """
         self.assertEqual(access_nested_map(nested_map, path), expctd_rslts)
+    
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map: Dict,
+                               path: Tuple[str]) -> None:
+        """
+        Use the assertRaises context manager
+        to test that a KeyError is raised
+        for the following inputs:
+            (use @parameterized.expand):
+            nested_map={}, path=("a",)
+            nested_map={"a": 1}, path=("a", "b")
+        """
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
 
 
 if __name__ == '__main__':
